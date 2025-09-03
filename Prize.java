8fcde18100dfbc7b3795798f410f486e027b2f73
GithubMaahi <star.maheshp1414@gmail.com>
2025-09-03 23:51:59 +0530

    Added .gitignore and updated Prize.java

--- Prize.java
+++ Prize.java
@@ -1,18 +1,19 @@
-
-
 import java.util.Scanner;
+
 public class Prize {
-    public static void main(String[]args){
-        Scanner sc =new Scanner (System.in);
-        int cash=sc.nextInt();
-        if (cash<10 ){
-            System.out.println("can not buy anything");
-        }
-        else if (cash>10&&cash<50){
-            System.out.println("canbuy one thing");
-        }
-        else{
-            System.out.println("can buy anything");
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter cash amount: ");
+            int cash = sc.nextInt();
+
+            if (cash < 10) {
+                System.out.println("x: can not buy anything");
+            } else if (cash >= 10 && cash < 50) {
+                System.out.println("x: can buy one thing");
+            } else {
+                System.out.println("x: can buy anything");
+            }
         }
     }
 }
+
